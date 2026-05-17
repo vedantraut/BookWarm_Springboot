42fd1923a633a0dd1768fdd0ea3ac8fe9d5b0da1
Vedant Raut <vedantraut49@gmail.com>
2026-05-17 20:28:50 +0530

    Admin APIs to add, get Coffees, Books, Authors

--- Book-Warm-Application/src/main/java/com/vedantraut/bookwarm/controllers/AdminController.java
+++ Book-Warm-Application/src/main/java/com/vedantraut/bookwarm/controllers/AdminController.java
@@ -1,6 +1,7 @@
 package com.vedantraut.bookwarm.controllers;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.vedantraut.bookwarm.dtos.AuthenticationRequest;
 import com.vedantraut.bookwarm.dtos.AuthenticationResponse;
+import com.vedantraut.bookwarm.dtos.AuthorDTO;
 import com.vedantraut.bookwarm.dtos.BookDTO;
+import com.vedantraut.bookwarm.dtos.CoffeeDTO;
 import com.vedantraut.bookwarm.dtos.DashboardStatsDTO;
 import com.vedantraut.bookwarm.dtos.UserDTO;
 import com.vedantraut.bookwarm.services.AdminService;
 import com.vedantraut.bookwarm.services.AuthenticationService;
+import com.vedantraut.bookwarm.services.AuthorService;
 import com.vedantraut.bookwarm.services.UserService;
 
 import jakarta.validation.Valid;
@@ -35,6 +39,7 @@ public class AdminController {
 	@Autowired
 	AdminService adminservice;
 	
+	
 	@GetMapping("/dashboard")
 	public String dashboard() {
 		return "Welcome Admin!";
@@ -46,13 +51,52 @@ public class AdminController {
 	}
 	
 	@PostMapping("/books")
-	public ResponseEntity<BookDTO> addBooks(@RequestBody BookDTO bookdto) {
+	public ResponseEntity<BookDTO> addBook(@RequestBody @Valid BookDTO bookdto) {
 		
 		BookDTO b = adminservice.addBook(bookdto);
 		
 		return ResponseEntity.ok(b);
 	}
 	
+	@PostMapping("/coffees")
+	public ResponseEntity<CoffeeDTO> addCoffee(@RequestBody @Valid CoffeeDTO coffeedto) {
+		
+		CoffeeDTO c = adminservice.addCoffee(coffeedto);
+		
+		return ResponseEntity.ok(c);
+	}
+	
+	@PostMapping("/authors")
+	public ResponseEntity<AuthorDTO> addAuthor(@RequestBody @Valid AuthorDTO authordto) {
+		
+		AuthorDTO a = adminservice.addAuthor(authordto);
+		
+		return ResponseEntity.ok(a);
+	}
+	
+	@GetMapping("/books")
+	public ResponseEntity<List<BookDTO>> getBooks() {
+		
+		List<BookDTO> listOfAllBooks = adminservice.getBooks();
+		
+		return ResponseEntity.ok(listOfAllBooks);
+	}
+	
+	@GetMapping("/coffees")
+	public ResponseEntity<List<CoffeeDTO>> getCoffees() {
+		
+		List<CoffeeDTO> listOfAllCoffee = adminservice.getCoffees();
+		
+		return ResponseEntity.ok(listOfAllCoffee);
+	}
+	
+	@GetMapping("/authors")
+	public ResponseEntity<List<AuthorDTO>> getAuthors() {
+		
+		List<AuthorDTO> listOfAllAuthors = adminservice.getAuthors();
+		
+		return ResponseEntity.ok(listOfAllAuthors);
+	}
 	
 	
 	
--- Book-Warm-Application/src/main/java/com/vedantraut/bookwarm/services/AdminService.java
+++ Book-Warm-Application/src/main/java/com/vedantraut/bookwarm/services/AdminService.java
@@ -1,15 +1,23 @@
 package com.vedantraut.bookwarm.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.vedantraut.bookwarm.dtos.AuthorDTO;
 import com.vedantraut.bookwarm.dtos.BookDTO;
+import com.vedantraut.bookwarm.dtos.CoffeeDTO;
 import com.vedantraut.bookwarm.dtos.DashboardStatsDTO;
+import com.vedantraut.bookwarm.entity.Author;
 import com.vedantraut.bookwarm.entity.Book;
+import com.vedantraut.bookwarm.entity.Coffee;
 import com.vedantraut.bookwarm.repository.BookRepository;
 import com.vedantraut.bookwarm.repository.OrderRepository;
 import com.vedantraut.bookwarm.repository.UserRepository;
+
+import jakarta.validation.Valid;
 
 @Service
 public class AdminService {
@@ -25,6 +33,12 @@ public class AdminService {
     
     @Autowired
 	private BookService bookservice;
+    
+    @Autowired
+    private CoffeeService coffeeservice;
+    
+    @Autowired
+	AuthorService authorservice;
 
 	public DashboardStatsDTO getDashboardStats() {
 		
@@ -42,6 +56,44 @@ public class AdminService {
 		bookdto.setId(b.getBookId());
 		
 		return bookdto;
+	}
+
+	public CoffeeDTO addCoffee(CoffeeDTO coffeedto) {
+		
+		Coffee c = coffeeservice.saveCoffee(coffeedto);
+		
+		coffeedto.setCoffeeId(c.getCoffeeId());
+		
+		return coffeedto;
+	}
+
+	public AuthorDTO addAuthor(@Valid AuthorDTO authordto) {
+		
+		Author a = authorservice.saveAuthor(authordto);
+		
+		authordto.setId(authordto.getId());
+		
+		return null;
+	}
+
+	public List<BookDTO> getBooks() {
+		List<BookDTO> books = bookservice.getAllBooks();
+		
+		return books;
+	}
+
+	public List<CoffeeDTO> getCoffees() {
+		List<CoffeeDTO> coffee = coffeeservice.fetchAllCoffee();
+		
+		return coffee;
+	}
+
+	public List<AuthorDTO> getAuthors() {
+//		List<AuthorDTO> authors = authorservice.getAllAuthors();
+		
+//		return authors;
+		
+		return null;
 	}
 	
 	
